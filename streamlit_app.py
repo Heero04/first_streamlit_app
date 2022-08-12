752b405448bb73775a0fc299cba167c0c71bd37d
Heero04 <36018263+Heero04@users.noreply.github.com>
2022-08-12 18:30:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -8,25 +13,23 @@ streamlit.text(' 👈 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' Kale, Spinach & Rocket Smoothie')
 streamlit.text(' Hard-bolied Free-Range Egg')
 
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-
 streamlit.dataframe(fruits_to_show)
-
 
 #New Section to display fruit vice API Response
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -37,7 +40,6 @@ streamlit.dataframe(fruityvice_normalized)
 # dont run anything pass here troubleshot
 streamlit.stop()
 
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
